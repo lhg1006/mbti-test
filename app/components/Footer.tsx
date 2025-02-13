9faba474c72ed0a08278334ff51cff89ced0c80e
이효규 <lhg961006@gmail.com>
2025-02-13 15:53:51 +0900

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 w-full py-4 text-center text-sm text-gray-500 bg-transparent">
-      <p>© {new Date().getFullYear()} lhg1006. All rights reserved.</p>
+      <p>© 2025 lhg1006. All rights reserved.</p>
     </footer>
   )
 } 
